99e76b9d3963bf448d7a52bcd7f64a78620dd3fb
ayato <ayato.211210@gmail.com>
2026-08-02 23:16:19 +0900

    Gameクラス調整

--- src/bingo/Game.java
+++ src/bingo/Game.java
@@ -27,34 +27,28 @@ public class Game {
 
 	public static void start5x5(Scanner scanner) {
 		Menu.printTitle("5 × 5 BINGO");
-		if (player == null) {
-			System.out.print("プレイヤー名 ▶ ");
-			String name = scanner.nextLine();
-			player = new Player(name);
-		}
-		System.out.println("\nようこそ、" + player.getName() + "さん");
+		setUpPlayer(scanner);
 		BingoCard.showCard(5);
 	}
 
 	public static void start3x3(Scanner scanner) {
 		Menu.printTitle("3 × 3 BINGO");
-		if (player == null) {
-			System.out.print("プレイヤー名 ▶ ");
-			String name = scanner.nextLine();
-			player = new Player(name);
-		}
-		System.out.println("\nようこそ、" + player.getName() + "さん");
+		setUpPlayer(scanner);
 		BingoCard.showCard(3);
 	}
 
 	public static void start7x7(Scanner scanner) {
 		Menu.printTitle("7 × 7 BINGO");
+		setUpPlayer(scanner);
+		BingoCard.showCard(7);
+	}
+
+	private static void setUpPlayer(Scanner scanner) {
 		if (player == null) {
 			System.out.print("プレイヤー名 ▶ ");
 			String name = scanner.nextLine();
 			player = new Player(name);
 		}
 		System.out.println("\nようこそ、" + player.getName() + "さん");
-		BingoCard.showCard(7);
 	}
 }
